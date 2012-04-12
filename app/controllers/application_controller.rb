2e3119bcc8b9d51e43d0cb7e609d523120355336
Daniel Lopes <danielvlopes@gmail.com>
2012-04-11 21:40:04 -0300

    trabalhando com locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   respond_to :html, :json, :xml, :js
+  
+  before_filter :set_locale
+  
+private
+  
+  def set_locale
+    I18n.locale = params[:locale] || :en
+  end
+  
 end
